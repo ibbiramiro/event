7f653a7ec0065f756c588ac7a852cb4457efc59d
ibbiramiro <ibbiramiro@gmail.com>
2026-06-15 21:49:00 +0700

    Ten commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,13 @@ export const metadata: Metadata = {
   description: "Management System Dashboard",
 };
 
+export const viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
